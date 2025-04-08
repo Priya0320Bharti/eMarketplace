d167e30b152176687012bde16c8a4de99140c65f
m-triple-m <triplem656@gmail.com>
2025-04-08 20:54:00 +0530

    fix

--- frontend/src/app/user/layout.jsx
+++ frontend/src/app/user/layout.jsx
@@ -1,8 +1,10 @@
+import Navbar from '@/components/Navbar';
 import React from 'react'
 
 const Layout = ({ children }) => {
     return (
         <>
+            <Navbar />
             {children}
         </>
     )
